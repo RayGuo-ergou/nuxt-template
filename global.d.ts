e810728e8c1156c184224bcf5237488b7e9636b0
Ray Guo <ma2808203259@hotmail.com>
2023-10-10 22:06:53 +1100

    fix: fix prisma with any type

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,12 @@
-declare module globalThis {
+import type { PrismaClient } from '@prisma/client'
+
+/**
+ * @info To declare types for globalThis variables, define with `declare global`
+ * @see https://stackoverflow.com/a/64197006
+ * @see https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#type-checking-for-globalthis
+ */
+declare global{
+
   // eslint-disable-next-line
   var prisma: PrismaClient | undefined
 }
