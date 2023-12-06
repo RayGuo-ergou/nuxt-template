251bd646920c50dbb6f73fc9ee2e69e791165989
Ray Guo <ma2808203259@hotmail.com>
2023-12-06 21:07:10 +1100

    chore: bump pkgs

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
 import { defaultConfig, ray } from '@ergou_rrrr/eslint-config'
-import unocss from '@unocss/eslint-config/flat'
 
-export default await ray(defaultConfig, unocss)
+export default ray(defaultConfig)
